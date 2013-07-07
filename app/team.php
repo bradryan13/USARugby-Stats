e9a5a465bb3e9fee06e293d1331bbeb7ae94eb8b
Bradley Ryan <bryan@usarugby.org>
2013-07-07 13:56:32 -0600

    Team Games View

--- app/team.php
+++ app/team.php
@@ -18,38 +18,45 @@ if ($request->get('refresh')) {
     }
 }
 ?>
-<div class="row">
-    <div class="span1">
-        <form method="post" action="team.php?id=<?php echo $team_id; ?>">
-            <input type='hidden' name='refresh' value='1' />
-            <button type="submit" class="btn btn-large btn-success"><i class="icon-refresh icon-white"></i></button>
-        </form>
+<div class="header">
+	<div class="container">
+	<div class="team-logo">
+				<!-- Problems with AllPlayers SSL Certificate  -->
+				<?php echo "<img src='$team_logo' alt='{$team['name']}'/>";  ?>
+	</div>
+    <div class="team-name">
+        <?php echo "<h1 class='w-logo'>{$team['name']}</h1>"; ?>
     </div>
-    <div class="span1">
-        <?php echo "<img src='$team_logo' alt='{$team['name']}' class='img-polaroid group-logo' onerror='imgError(this);'/>"; ?>
-    </div>
-    <div class="span10">
-        <?php echo "<h1>{$team['name']}</h1>"; ?>
-    </div>
+	</div>
 </div>
+
+<div class="container">
+<div id="maincontent">
+
 <?php
 
+
+/* 
 echo "<h2>Event Rosters</h2>";
 //Get the rosters for this team
 include_once './team_event_rosters.php';
 echo "<br/>";
+*/
 
-echo "<h2>Games</h2>";
-//Get the rosters for this team
+//Get the Games for this team
 include_once './team_games.php';
-echo "<br/>";
 
+/*
 echo "<h2>Game Rosters</h2>";
 //Get the rosters for this team
 echo "<div class='rosters-wrapper'>";
 include_once './team_game_rosters.php';
 echo "<br/>";
 echo "</div>";
+*/
 
 include_once './footer.php';
-mysql_close();
+mysql_close(); ?>
+
+</div>
+</div>
--- app/team_games.php
+++ app/team_games.php
@@ -5,75 +5,130 @@ $team_uuid = $request->get('team_uuid');
 include_once './include.php';
 
 if (!empty($team_uuid)) {
-    $team_id = $db->getSerialIDByUUID('teams', $team_uuid);
-    $team = $db->getTeam($team_id);
-    $game_rows = array();
+	$team_id = $db->getSerialIDByUUID('teams', $team_uuid);
+	$team = $db->getTeam($team_id);
+	$game_rows = array();
 }
 
 if (empty($team_id)) {
-    $team_id = $request->get('team_id');
+	$team_id = $request->get('team_id');
 }
 
-?>
-<div id="wrapper" class="container-fluid team-games">
-  <?php
-    $team_games = $db->getTeamGames($team_id);
-    // If we're not looking at a team - we may be on a competition (top level)
-    //  related group - try to look it up.
-    if (empty($team_games)) {
-        // team_uuid in this case would be comp_uuid
-        $team_games = $db->getCompetitionGames($team_uuid);
-    }
-    if (empty($team_games)) {
-        echo '<!-- No Games -->';
-    }
-    else {
-        // Regular display.
-        if (empty($iframe)) {
-            foreach ($team_games as $team_game) {
-                $kickoff = date('M d - g:ia', strtotime($team_game['kickoff']));
-                echo "<a href='game.php?id={$team_game['id']}'>";
-                echo teamNameNL($team_game['away_id'])." @ ".teamNameNL($team_game['home_id'])." - $kickoff</a><br/>";
-           }
-        }
-      // Iframe.
-        else {
-            if (!empty($iframe)) {
-                    $pre_date = '';
-                foreach ($team_games as $team_game) {
-                    $game = array();
-                    $resource = $db->getResource($team_game['field_num']);
-                    $loc_url = getResourceMapUrl($resource);
-                    $game['comp_game_id'] = $team_game['comp_game_id'];
-                    $game['kickoff'] = date('l M d, Y', strtotime($team_game['kickoff']));
-                    $game['kickoff_time'] = date('g:ia', strtotime($team_game['kickoff']));
-                    $game['score'] = "<b>" . "{$team_game['home_score']} - {$team_game['away_score']}" . "</b>";
-                    $game['away_id'] = teamNameNL($team_game['away_id']);
-                    $game['home_id'] = teamNameNL($team_game['home_id']);
-                    $game['field'] = "<a href=" . "$loc_url" . ">" . "{$resource['title']}" . "</a>";
-                    $game['league'] = $team_game['league_type'];
-                    $game['pre_date'] = false;
-                    if ($pre_date != $game['kickoff']){
-                        $pre_date = $game['kickoff'];
-                        $game['pre_date'] = true;
-                        }
-                    else {
-                        $game['kickoff'] =  '';
-                    }
-                    $game_rows[] = $game;
-                }
-            }
-            
-            if (empty($twig)) {
-                $loader = new Twig_Loader_Filesystem(__DIR__.'/views');
-                $twig = new Twig_Environment($loader, array());
-            }
-            echo $twig->render('comp-games.twig', array('gamerows' => $game_rows));
-      }
 
-    }
+?>
+<div>
+<script type="text/javascript" charset="utf-8">		
+$(document).ready(function(){
+  $('#sort-games').dataTable({
+    "bAutoWidth": true,
+    "aoColumnDefs": [
+			{ 
+				"bSortable": false, 
+				 "aTargets": [ -1 ] // <-- gets last column and turns off sorting
+			} ]
+  })
+    .yadcf([
+    {column_number : 0, data:["2013","2012"], filter_container_id: "external_filter_container", filter_default_label: "All Seasons", filter_reset_button_text: false}
+    ]);
+    $('.dataTables_filter input').attr('placeholder', 'Filter Games');
+	$('div.dataTables_filter input').focus()
+});
+</script>
+
+<div id="external_filter_container" class="no-clear-filter"></div>
+	
+
+  <?php
+$team_games = $db->getTeamGames($team_id);
+// If we're not looking at a team - we may be on a competition (top level)
+//  related group - try to look it up.
+if (empty($team_games)) {
+	// team_uuid in this case would be comp_uuid
+	$team_games = $db->getCompetitionGames($team_uuid);
+}
+if (empty($team_games)) {
+	echo '<!-- No Games -->';
+}
+else {
+
+
+	// Regular display.
+	if (empty($iframe)) {
+		echo "<table class='games-table' id='sort-games'><thead><th>Date</th><th>Home Team</th><th>Result/Time</th><th>Away Team</th><th>Type</th><th></th></thead>";
+		foreach ($team_games as $team_game) {
+			$game['score'] = "{$team_game['home_score']} - {$team_game['away_score']}";
+			$game['kickoff'] = date('l M d, Y', strtotime($team_game['kickoff']));
+			$kickofftime = date('g:i A', strtotime($team_game['kickoff']));
+			$kickoffdate = date('D, M d, Y', strtotime($team_game['kickoff']));
+			$game['league'] = $team_game['league_type'];
+			echo "<tr>";
+			echo "<td>" . $kickoffdate. "</td>";
+			echo "<td class='home-team'>" . teamNameNL($team_game['home_id']) . "</td>";
+			echo "<td class='score-time'><a href='game.php?id={$team_game['id']}'>";
+			
+			// If game score has not been entered, replace 0-0 with N/A
+			if($team_game['away_score'] == "0" && $team_game['home_score'] == "0") {
+				$game['score'] = "N/A";
+			}
+			// If game date has not passed, show game instead of score
+			if (strtotime($kickoffdate) < time()) {
+				echo $game['score'];
+			}
+			else {
+				echo $kickofftime;
+			}
+			echo "</a></td>";
+			echo "<td>" . teamNameNL($team_game['away_id']) . "</td>";
+			echo "<td>". $game['league'] . "</td>";
+			echo "<td>
+					<div class='btn-group pull-right'>
+						<a class='btn dropdown-toggle' data-toggle='dropdown' href='#'><i class='icon-cog'></i><span class='caret'></span></a>
+							<ul class='dropdown-menu'>
+								<li><a href='game.php?id={$team_game['id']}'><i class='icon-edit'></i> Edit</a></li>
+								<li><a href='game_roster.php?gid={$team_game['id']}'><i class='icon-list'></i> Rosters</a></li>
+							</ul></div></td>";
+			echo "</tr>";
+		}
+		echo "</table>";
+	}
+	// Iframe.
+	else {
+		if (!empty($iframe)) {
+			$pre_date = '';
+			foreach ($team_games as $team_game) {
+				$game = array();
+				$resource = $db->getResource($team_game['field_num']);
+				$loc_url = getResourceMapUrl($resource);
+				$game['comp_game_id'] = $team_game['comp_game_id'];
+				$game['kickoff'] = date('l M d, Y', strtotime($team_game['kickoff']));
+				$game['kickoff_time'] = date('g:ia', strtotime($team_game['kickoff']));
+				$game['score'] = "<b>" . "{$team_game['home_score']} - {$team_game['away_score']}" . "</b>";
+				$game['away_id'] = teamNameNL($team_game['away_id']);
+				$game['home_id'] = teamNameNL($team_game['home_id']);
+				$game['field'] = "<a href=" . "$loc_url" . ">" . "{$resource['title']}" . "</a>";
+				$game['pre_date'] = false;
+				if ($pre_date != $game['kickoff']){
+					$pre_date = $game['kickoff'];
+					$game['pre_date'] = true;
+				}
+				else {
+					$game['kickoff'] =  '';
+				}
+				$game_rows[] = $game;
+			}
+		}
+
+		if (empty($twig)) {
+			$loader = new Twig_Loader_Filesystem(__DIR__.'/views');
+			$twig = new Twig_Environment($loader, array());
+		}
+		echo $twig->render('comp-games.twig', array('gamerows' => $game_rows));
+	}
+
+}
 ?>
 </div>
 <?php
+    
 
 include './footer.php';
