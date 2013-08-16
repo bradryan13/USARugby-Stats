f1c019b53f9c6b63402a89a92d4998c2e75fe99a
root <root@li614-102.members.linode.com>
2013-08-16 18:27:12 +0000

    Fixed standing and edit roster by specifing no redis user and pass.

--- app/other_funcs.php
+++ app/other_funcs.php
@@ -76,7 +76,7 @@ function updateScore($game_id)
     $query = "UPDATE `games` SET home_score='$homep', away_score='$awayp' WHERE id = '$game_id'";
     $result = mysql_query($query);
     include './config.php';
-    Resque::setBackend('redis://redis:' . $config['redis_password'] . '@' . $config['redis_host']);
+    //Resque::setBackend('redis://redis:' . $config['redis_password'] . '@' . $config['redis_host']);
 
     $db = new DataSource;
     $home_team = $db->getTeam($home_id);
